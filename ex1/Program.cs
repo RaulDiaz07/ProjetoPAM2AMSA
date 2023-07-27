24760c45fa6a785a5165db9959e29a1e80483e39
RaulDiaz07 <140725104+RaulDiaz07@users.noreply.github.com>
2023-07-27 11:15:30 -0300

    Add files via upload

---
+++ ex1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/ information
+int n1;
+int n2;
+Console.WriteLine("Digite os numeros a serem comparados");
+n1 = Convert.ToInt32(Console.ReadLine());
+n2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+if(n1<n2){
+Console.WriteLine($"O maior numero dentre os dois é o {n2}");
+
+}
+
+else if(n1==n2){
+    Console.WriteLine("Os Numeros são iguais, não a Diferenças");
+}
+else Console.WriteLine($"O maior numero dentre os dois é o {n1}");
+
+
